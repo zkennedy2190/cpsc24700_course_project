5128a42e217d2a09094917809042670da09ed2e8
zkennedy2190 <zachariahckennedy@lewisu.edu>
2025-07-05 20:26:20 -0500

    Fix more API issues

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
-// frontend/src/config.js
-
-const API_BASE_URL = "https://cpsc24700-course-project-backend-g8chephmg4brc8f8.centralus-01.azurewebsites.net";
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? "https://cpsc24700-course-project-backend-g8chephmg4brc8f8.centralus-01.azurewebsites.net"
+    : "http://localhost:5000";
 
 export default API_BASE_URL;
